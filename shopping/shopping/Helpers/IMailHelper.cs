e7c405fb89b37ebfed00b0525f9870701b0062ca
santiago282389 <santi-arias-@outlook.com>
2022-05-19 09:14:53 -0500

    Confirm emal and recover password

---
+++ shopping/shopping/Helpers/IMailHelper.cs
@@ -0,0 +1,9 @@
+﻿using shopping.Common;
+
+namespace shopping.Helpers
+{
+    public interface IMailHelper
+    {
+        Response SendMail(string toName, string toEmail, string subject, string body);
+    }
+}
---
+++ shopping/shopping/Models/RecoverPasswordViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace shopping.Models
+{
+    public class RecoverPasswordViewModel
+    {
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [EmailAddress(ErrorMessage = "Debes ingresar un correo válido.")]
+        public string Email { get; set; }
+    }
+}
